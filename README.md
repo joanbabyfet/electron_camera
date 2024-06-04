0f9bf62ae5eb773fa01174495b1e69138ebef200
alan <crwu0206@gmail.com>
2024-06-04 19:36:13 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+## About
+electron 实现调用外接摄像头
+
+## Usage
+```
+# 执行
+npm install
+npm start
+```
+
+## Requires
+npm 8.19.2
+
+## Maintainers
+Alan
+
+## LICENSE
+[MIT License](https://github.com/joanbabyfet/electron_camera/blob/master/LICENSE)
---
+++ main.js
@@ -0,0 +1,31 @@
+//入口文件
+const { app, BrowserWindow } = require('electron')
+const url = require('url')
+const path = require('path')
+
+//屏蔽安全告警
+process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true'
+
+// Set the path where recordings will be saved
+app.setPath("userData", __dirname + "/saved_recordings")
+
+function createWindow() {
+  //创建一个浏览器窗口
+  mainWindow = new BrowserWindow({
+    width: 800, 
+    height: 600,
+    //autoHideMenuBar: true,
+    alwaysOnTop: true, //指定窗口是否始终显示在其他窗口之上
+    skipTaskbar: true, //让应用不会出现在任务栏中
+    //frame: false, //false为创建无边框窗口
+  })
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+  //开启开发人员工具
+  //mainWindow.openDevTools()
+}
+//在 Electron 中，只有在 app 模块的 ready 事件被激发后才能创建浏览器窗口
+app.on('ready', createWindow)
